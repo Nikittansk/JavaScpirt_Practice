f3642df96eb59b851f736975b287140f59b2bfeb
Никита Носоков <61756114+Nikittansk@users.noreply.github.com>
2021-06-22 20:13:29 +0300

    Changes 22.06.2021

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 // Для основной разработке
 // Вызов функции. $ - означает, что это плагин
-const modal = $.modal()
+const modal = $.modal({
+    title: 'Nikita Modal',
+    closable: true,
+    content: `
+        <p>Lorem ipsum dolor sit.</p>
+    `,
+    width: '400px',
+    // FooterButtons - массив с кнопками
+    // Text - название кнопки
+    // Type - стиль
+    // Handler() - что будет происходить при клике
+    footerButtons: [
+        {text: 'Ok', type: 'primary', handler() {
+            console.log('Primary btn clicked')
+        }},
+        {text: 'Cancel', type: 'danger', handler() {
+            console.log('Danger btn clicked')
+        }}
+    ]
+})
--- plugins/modal.js
+++ plugins/modal.js
@@ -1,10 +1,39 @@
-// Объект у которого есть функция modal, которая является функцией
-/* Функция будет в себя принимать некий объект options и возвращать объект
- с методами */
-// Open - открыть
-// Close - закрыть
-// Destroy - оптимизация
+// Функция для вставки элемента
+Element.prototype.appendAfter = function(element) {
+    element.parentNode.insertBefore(this, element.nextSibling)
+}
+
+function noop() {}
+
+function _createModalFooter(buttons = []) {
+    if (buttons.length === 0) {
+        return document.createElement('div')
+    }
+
+    const wrap = document.createElement('div')
+    wrap.classList.add('modal-footer')
+
+    // Создание кнопок
+    for(const btn of buttons){
+        const $btn = document.createElement('button')
+        // Текст
+        $btn.textContent = btn.text
+        // Bootstrap класс
+        $btn.classList.add('btn')
+        // Bootstrap стиль
+        $btn.classList.add(`btn-${btn.type || 'secondary'}`)
+        // Действия при клике
+        $btn.onclick = btn.handler || noop
+
+        // Помещение в нужное место
+        wrap.appendChild($btn)
+    }
+
+    return wrap
+}
+
 function _createModal(options) {
+    const DEFAULT_WIDTH = '600px'
     // Создание элемента в HTML-документе с указаным тегом
     const modal = document.createElement('div')
     // Добавление класса к созданному элементу
@@ -12,33 +41,38 @@ function _createModal(options) {
     // Вставляем элемент в нужное место
     modal.insertAdjacentHTML('afterbegin', `
     <!-- Затемнение фона -->
-    <div class="modal-overlay">
+    <div class="modal-overlay" data-close="true">
         <!-- Главный элемент для самого модального окна -->
-        <div class="modal-window">
+        <div class="modal-window" style="width: ${options.width || DEFAULT_WIDTH}">
             <!-- Элементы модальног окна -->
             <div class="modal-header">
-                <span class="modal-title">Modal title</span>
-                <span class="modal-close">&times;</span>
+                <span class="modal-title">${options.title || ''}</span>
+                ${options.closable ? `<span class="modal-close" data-close="true">&times;</span>` : ''}
+                
             </div>
 
-            <div class="modal-body">
-                <p>Lorem ipsum dolor sit.</p>
-                <p>Lorem ipsum dolor sit.</p>
+            <div class="modal-body" data-content>
+                ${options.content || ''}
             </div>
 
-            <div class="modal-footer">
-                <button>Ok</button>
-                <button>Cancel</button>
-            </div>
+
         </div>
     </div>
     `)
+    const footer = _createModalFooter(options.footerButtons)
+    // Вставка footer
+    footer.appendAfter(modal.querySelector('[data-content]'))
     // Созданный элемент вставляем в конец document body
     // Без этой строчке, при вызове метода $.modal, модальное окно не отображается
     document.body.appendChild(modal)
     return modal
 }
-
+// Объект у которого есть функция modal, которая является функцией
+/* Функция будет в себя принимать некий объект options и возвращать объект
+ с методами */
+// Open - открыть
+// Close - закрыть
+// Destroy - оптимизация
 $.modal = function (options) {
     // Время за которое будет идти анимация
     const ANIMATION_SPEED = 200
@@ -46,8 +80,14 @@ $.modal = function (options) {
     // Защита от вызова метода open, когда сработал метода close
     let closing = false
 
-    return {
+    let destroyed = false
+
+    const modal = {
         open() {
+            if (destroyed) {
+                // Проверка был ли использован метод destroyed
+                return console.log('Modal is destroyed')
+            }
             // Добавляем класс
             !closing && $modal.classList.add('open')
          },
@@ -61,7 +101,30 @@ $.modal = function (options) {
                 $modal.classList.remove('hide')
                 closing = false
             }, ANIMATION_SPEED)
-         },
-        destroy() { }
+         }
     }
+
+    // Функцию записали в переменную, чтобы ее легче было использовать (добавлять, удалять)
+    const listener = event => {
+        if (event.target.dataset.close) {
+            modal.close()
+        }
+    }
+
+    // Закрытие модального окна
+    $modal.addEventListener('click', listener)
+
+    return Object.assign(modal, {
+        destroy() {
+            // Очистка DOM от modal
+            $modal.parentNode.removeChild($modal)
+            $modal.removeEventListener('click', listener)
+            destroyed = true
+        },
+        
+        // Смена контента у модального окна
+        setContent(html) {
+            $modal.querySelector('[data-content]').innerHTML = html
+        }
+    })
 }
